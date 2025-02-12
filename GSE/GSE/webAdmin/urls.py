32454b67a289e6aec5576b1f1851a92dc9c43dce
t5z4s6 <tillyzuzu@gmail.com>
2025-02-12 10:12:48 +0000

    creation of initial html templates
    html files created with views and urls edited to reflect this

--- GSE/GSE/webAdmin/urls.py
+++ GSE/GSE/webAdmin/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+from . import views
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="home"),
+    path("home", views.showStats, name="home"),
+    path("calculate", views.calculateRiskPage, name="calculateRiskPage"),
+    path("calculationExplain", views.calculationExplain, name="calculationExplain"),
 ]
--- GSE/GSE/webAdmin/views.py
+++ GSE/GSE/webAdmin/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "home.html")
 # Create your views here.
