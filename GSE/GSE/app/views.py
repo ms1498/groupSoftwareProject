c34c6886abfc28560080de42434847318c713655
t5z4s6 <tillyzuzu@gmail.com>
2025-02-12 10:56:14 +0000

    renaming files + nav bar

---
+++ GSE/GSE/app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, "home.html")
+
+def index(request):
+    return render(request, "discover.html")
+
+def index(request):
+    return render(request, "home.html")
+
+def index(request):
+    return render(request, "home.html")
+
+def index(request):
+    return render(request, "home.html")
--- GSE/GSE/webAdmin/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
-
-
-def index(request):
-    return render(request, "home.html")
