fe6cf08ccbf71493299247f53335bc35c59fddaf
CharDryExe <rd634@exeter.ac.uk>
2025-02-13 20:41:46 +0000

    Added initial models
    Co-Authored-By: Lia <196816299+Ven-rdm@users.noreply.github.com>

--- GSE/GSE/app/models.py
+++ GSE/GSE/app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(User)
+    points = models.IntegerField()
+
+class Developer(models.Model):
+    user = models.OneToOneField(User)
+
+class SocietyRepresentative(models.Model):
+    user = models.OneToOneField(User)
+    societyName = models.CharField(max_length=50)
+
+class Moderator(models.Model):
+    user = models.OneToOneField(User)
+
+class Event(models.Model):
+    eventType = models.CharField(max_length=50)
+    organiser = models.ForeignKey(SocietyRepresentative, on_delete=models.CASCADE)
+    eventDate = models.DateField()
+    eventTime = models.TimeField()
+    eventLocation = models.ForeignKey(Location, on_delete=models.CASCADE)
+    expectedAttendance = models.IntegerField(null=True)
+    actualAttendance = models.IntegerField(null=True)
+    maximumAttendance = models.IntegerField(null=True)
+
+class Location(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    address = models.CharField(max_length=255)
