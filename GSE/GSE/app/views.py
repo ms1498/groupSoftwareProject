6e7a29466627911170bfd6f4749dcdc10154b384
t5z4s6 <tillyzuzu@gmail.com>
2025-02-14 09:33:42 +0000

    organise page
    here is all the code for organising events

--- GSE/GSE/app/views.py
+++ GSE/GSE/app/views.py
@@ -3,8 +3,6 @@ from django.template import loader
 from django.http import HttpResponse
 
 import os
-<<<<<<< Updated upstream
-=======
 
 def serve_events_txt(request):
     file_path = os.path.join(os.path.dirname(__file__), 'templates', 'events.txt')
@@ -14,17 +12,7 @@ def serve_events_txt(request):
         return HttpResponse(content, content_type="text/plain")
     except FileNotFoundError:
         return HttpResponse("File not found", status=404)
->>>>>>> Stashed changes
 
-def serve_events_txt(request):
-    file_path = os.path.join(os.path.dirname(__file__), 'templates', 'events.txt')
-    try:
-        with open(file_path, 'r') as file:
-            content = file.read()
-        return HttpResponse(content, content_type="text/plain")
-    except FileNotFoundError:
-        return HttpResponse("File not found", status=404)
-    
 def index(request):
     return render(request, "home.html")
 
@@ -32,13 +20,13 @@ def discover(request):
     return render(request, "discover.html")
 
 def myEvents(request):
-    return render(request, "my-events.html")
+    return render(request, "myEvents.html")
 
 def organise(request):
     return render(request, "organise.html")
 
 def signIn(request):
-    return render(request, "sign-in.html")
+    return render(request, "signIn.html")
 
 def signUp(request):
-    return render(request, "sign-up.html")
+    return render(request, "signUp.html")
