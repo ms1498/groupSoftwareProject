235066bd2b9f4439aa811a1139a4ae5db1cf5874
loopymeow <mm1354@exeter.ac.uk>
2025-02-14 11:17:44 +0000

    Addes sign-in, sign-out and sign-up

--- GSE/GSE/app/urls.py
+++ GSE/GSE/app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("discover", views.discover, name="discover"),
-    path("myEvents", views.myEvents, name="myEvents"),
-    path("organise", views.organise, name="organise"),
-    path("signIn", views.signIn, name="signIn"),
-    path("signUp", views.signUp, name="signUp"),
+    path("discover/", views.discover, name="discover"),
+    path("myEvents/", views.myEvents, name="myEvents"),
+    path("organise/", views.organise, name="organise"),
+    path('signIn/', views.signInUser, name='signIn'),
+    path('signOut/', views.signOutUser, name='signOut'),
+    path("signUp/", views.signUpUser, name="signUp"),
 ]
--- GSE/GSE/app/views.py
+++ GSE/GSE/app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Group
+from .forms import SignInForm, SignupForm
 
 def index(request):
     return render(request, "home.html")
@@ -15,8 +17,54 @@ def myEvents(request):
 def organise(request):
     return render(request, "organise.html")
 
-def signIn(request):
-    return render(request, "signIn.html")
+#authentication section
+def signInUser(request):
+    """
+     Takes the username and password and validates whether it matches a user in the system, if so it logs them in.
+     @param     user's request
+     @return    renders home + session logged in if successful, keeps rendering sign-in page if unsuccessful 
+     @author    Maisie Marks
+    """
+    if request.method == "POST":
+        form = SignInForm(request.POST)
+        if form.is_valid() == True:
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user != None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'sign-in.html', {'form': form, 'error': 'Invalid username or password'})
+    else:
+        form = SignInForm()
+    return render(request, 'sign-in.html', {'form': form})
 
-def signUp(request):
-    return render(request, "signUp.html")
+def signOutUser(request):
+    """
+     Logs the user out of the current session and redirects them to the homepage.
+     @param     user's request
+     @return    renders homepage
+     @author    Maisie Marks
+    """
+    logout(request)
+    return redirect('/app')
+
+def signUpUser(request):
+    """
+     Allows the user to sign-up and make an account on the webpage.
+     @param     user's request
+     @return    renders the sign-up page (showing currently logged in user), adding an account to the system.
+     @author    Maisie Marks
+    """
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid() == True :
+            user = form.save()
+            group = Group.objects.get(name='student')
+            user.groups.add(group)      
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignupForm()
+    return render(request, 'sign-up.html', {'form': form})
