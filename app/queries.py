fa7981e01189cfffeab0f10f0c49e320be743426
Seth-Mallinson <sm1442@exeter.ac.uk>
2025-02-18 11:16:27 +0000

    largely done

--- app/queries.py
+++ app/queries.py
@@ -15,46 +15,43 @@ from mysite.qrgen import get_qrcode_from_response
 
 
 def serve_events(request:HttpRequest) -> HttpResponse:
+    """Returns a list of all events that match arbitrary criteria. Use a 'filters' parameter in the URL to specify a list[str] of comma separated conditions, for example ?filters=pk=1,event_location="forum"
+    @param user's request
+    @return a json list of events
+    @author Seth Mallinson"""
+
     if request.method == "GET":
-        data = serializers.serialize("json", Event.objects.all())
+        valid_events = Event.objects.all()
+        # attempt to apply filters, if there are any. We do this by executing a filter operation for however many filter arguments were provided.
+        try:
+            conditions:list[str] = request.GET["filters"].split(",")
+            for condition in conditions:
+                local_vars = {"valid_events":valid_events}
+                exec("valid_events = valid_events.filter(" + condition + ")", globals(), local_vars)
+                valid_events = local_vars["valid_events"]
+        except:
+            pass
+        data = serializers.serialize("json", valid_events)
         return HttpResponse(data, content_type="text/plain")
     else:
         return HttpResponse("Invalid request - should be GET", status=400)
     
 def submit_event(request:HttpRequest) -> HttpResponse:
-    """Include a json description of the event in the request body and it will be saved to the database."""
-    if User.objects.count() == 0:
-        user:User = User.objects.create_user(username="temp", email="temp@temp.temp", password="temp")
-    if SocietyRepresentative.objects.count() == 0:
-        rep:SocietyRepresentative = SocietyRepresentative()
-        rep.user = User.objects.get(username="temp")
-        rep.societyName = "john society"
-        rep.save()
-    if Location.objects.count() == 0:
-        location:Location = Location()
-        location.name = "aaaaa"
-        location.address = "woooooo"
-        location.save()
+    """Include a json description of the event in the request body and it will be saved to the database.
+    @param GET request
+    @return response status
+    @author Seth Mallinson"""
 
     if request.method == "POST":
         try:
-            next(serializers.deserialize("json", "[" + str(request.body)[2:-1] + "]")).save()
-            data = json.loads(str(request.body)[2:-1])
-            event:Event = Event()
-            event.start_key = data["startKey"]
-            event.end_key = data["endKey"]
-            event.event_type = data["eventType"]
-            event.organiser = SocietyRepresentative.objects.filter(user=User.objects.get(username="temp"))[0]
-            event.event_date = data["eventDate"]
-            event.event_time = data["eventTime"]
-            event.event_location = Location.objects.filter(name=data["eventLocation"])[0]
-            event.expected_attendance = data["expectedAttendance"]
-            event.actual_attendance = data["actualAttendance"]
-            event.maximum_attendance = data["actualAttendance"]
-            event.approved = data["approved"]
-            event.save()
+            # convert organiser name to model object PK before deserialising
+            dictionary:dict[str, any] = json.loads(str(request.body)[2:-1])
+            dictionary["fields"].update({"organiser": SocietyRepresentative.objects.filter(user=User.objects.get(username=dictionary["fields"]["organiser"]))[0].pk})
+            data = json.dumps(dictionary)
+            # save the new event
+            next(serializers.deserialize("json", "[" + data + "]")).save()
             return HttpResponse(status=200)
         except:
-            return HttpResponse("Invalid request - bad message body. A JSON definition of the event is expected. Got: " + str(request.body)[2:-1], status=400)
+            return HttpResponse("Invalid request - error during deserialisation. A JSON definition of the event is expected. Got: " + str(request.body)[2:-1] + " \nmake sure that the user and location referenced by the event definition exist.", status=400)
     else:
         return HttpResponse("Invalid request - should be POST", status=400)
