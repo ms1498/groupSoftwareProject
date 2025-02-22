3e9dd353a7017bb8fe27ff6efea9809b64285e53
ms1498 <ms1498@exeter.ac.uk>
2025-02-22 16:50:09 +0000

    Update app/urls.py
    Co-authored-by: ts822 <ts822@exeter.ac.uk>

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from . import queries
 urlpatterns = [
     path("", views.index, name="home"),
     path("events-data", queries.serve_events, name="serve-events"),
-    path('register_event/<int:event_id>/', views.register_event, name='register_event'),
+    path("register_event/<int:event_id>/", views.register_event, name="register_event"),
     path("qrgen", queries.qrgen, name="QR code"),
     path("discover", views.discover, name="discover"),
     path("my-events", views.my_events, name="my-events"),
