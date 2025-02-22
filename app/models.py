559d1a058aaf04a479058a4f9df392c8987ce4e1
ts822 <ts822@exeter.ac.uk>
2025-02-22 14:50:45 +0000

    Revert "Merge branch 'main' into feature/sample-tables"
    This reverts commit c31722cd6470a13d3fa98bd5035dfa4c3f2528e4, reversing
    changes made to 3674f5f2bdb13728735e5a690b4f7494359f995f.

--- app/models.py
+++ app/models.py
@@ -1,38 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
+from mysite.keygen import generate_random_key
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     points = models.IntegerField()
+    class Meta:
+        permissions = [("sign_up", "Can sign up for events")]
 
 class Developer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_superuser = True
 
 class SocietyRepresentative(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     society_name = models.CharField(max_length=50)
+    class Meta:
+        permissions = [("create_events","Can create events"),
+                      ("generate_qr", "Can generate QR codes for events they are running")]
 
 class Moderator(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    class Meta:
+        permissions = [("view_unapproved_events", "Can view currently unapproved events"),
+                       ("approve_events", "Can approve currently unapproved events")]
 
 class Location(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
     address = models.CharField(max_length=255)
 
 class Event(models.Model):
-    start_key = models.CharField(max_length=7)
-    end_key = models.CharField(max_length=7, null=True)
-    category = models.CharField(max_length=50)
-    organiser = models.ForeignKey(SocietyRepresentative, on_delete=models.CASCADE, null=True)
-    date = models.DateTimeField()
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=50)
-    expected_attendance = models.IntegerField(null=True)
-    actual_attendance = models.IntegerField(null=True)
-    maximum_attendance = models.IntegerField(null=True)
+    startKey = models.CharField(max_length=7, default=generate_random_key(7))
+    endKey = models.CharField(max_length=7, null=True, default=generate_random_key(7))
+    eventType = models.CharField(max_length=50)
+    organiser = models.ForeignKey(SocietyRepresentative, on_delete=models.CASCADE)
+    eventDate = models.DateField()
+    eventTime = models.TimeField()
+    eventLocation = models.ForeignKey(Location, on_delete=models.CASCADE)
+    expectedAttendance = models.IntegerField(null=True)
+    actualAttendance = models.IntegerField(null=True)
+    maximumAttendance = models.IntegerField(null=True)
     approved = models.BooleanField(default=False)
-    description = models.CharField(max_length=500, null=True)
-    image = models.ImageField(blank=True)
 
 class Booking(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "app.apps.UWeaveConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
