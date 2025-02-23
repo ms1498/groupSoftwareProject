42960755f4567f35e36bbf6e71aedae09f439a0b
ms1498 <ms1498@exeter.ac.uk>
2025-02-23 13:30:24 +0000

    Update app/views.py
    Co-authored-by: ts822 <ts822@exeter.ac.uk>

--- app/views.py
+++ app/views.py
@@ -26,10 +26,10 @@ def discover(request: HttpRequest) -> HttpResponse:
 
     @author  Tilly Searle
     """
-    search_query = request.GET.get('search_query', '')
-    event_date = request.GET.get('event_date', '')
-    category = request.GET.get('category', '')
-    society = request.GET.get('society', '')
+    search_query = request.GET.get("search_query", "")
+    event_date = request.GET.get("event_date", "")
+    category = request.GET.get("category", "")
+    society = request.GET.get("society", "")
 
     events = Event.objects.all()
 
