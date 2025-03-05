e8a7f526702274ba717afd7f69bc6df1957034e4
Lia <dgf201@exeter.ac.uk>
2025-03-05 14:40:39 +0000

    Updated for pylint standards

--- app/models.py
+++ app/models.py
@@ -86,6 +86,7 @@ class Booking(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     def update_expected_attendance(self):
+        """Update the expected_attendance field of the linked event when a booking is created or deleted"""
         bookings = Booking.objects.all()
         bookings = bookings.filter(event=self.event)
         self.event.expected_attendance = bookings.count()
