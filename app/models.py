396a8302349f0f421eabc8251a37bf9353b04014
Lia <dgf201@exeter.ac.uk>
2025-03-05 15:01:22 +0000

    Added Kito's suggestion
    Co-authored-by: ts822 <ts822@exeter.ac.uk>

--- app/models.py
+++ app/models.py
@@ -86,7 +86,7 @@ class Booking(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     def update_expected_attendance(self):
-        """Update the expected_attendance field of the linked event"""
+        """Update the expected_attendance field of the linked event."""
         bookings = Booking.objects.all()
         bookings = bookings.filter(event=self.event)
         self.event.expected_attendance = bookings.count()
