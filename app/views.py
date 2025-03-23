2c02dc3324ae290031290c282b80dedd6e1c646e
t5z4s6 <tillyzuzu@gmail.com>
2025-03-23 21:20:15 +0000

    made aesteci changes to leaderboard
    changed the leaderboard to made it more visually appealing and added badges to a users popup.

--- app/views.py
+++ app/views.py
@@ -537,6 +537,8 @@ def leaderboard(request: HttpRequest) -> HttpResponse:
     @author  Lia Fisher
     """
     all_students = Student.objects.all().order_by("-points")
+    badge_links = Award.objects.all()
+    badges = Badge.objects.all()
     students = all_students[:10]
     top_ten = True
     rank = -1
@@ -552,7 +554,9 @@ def leaderboard(request: HttpRequest) -> HttpResponse:
         "students": students,
         "top_ten": top_ten,
         "rank": rank,
-        "points": points
+        "points": points,
+        "badge_links": badge_links,
+        "badges": badges
     })
 
 @login_required
